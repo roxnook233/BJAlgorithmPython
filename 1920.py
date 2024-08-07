63ae4e65bba154c0ba3bad050fdafcd2ce45ae4b
roxnook233 <87252115+roxnook233@users.noreply.github.com>
2024-08-08 00:30:10 +0900

    Update 1920.py

--- 1920.py
+++ 1920.py
@@ -9,3 +9,11 @@ for item in mInps:
     if item in nInps: print(1)
     else : print(0)
 
+'''
+import sys
+
+data = sys.stdin.readlines()
+
+a = set(data[1].strip().split())
+print("\n".join(["1" if num in a else "0" for num in data[3].split()]))
+'''
