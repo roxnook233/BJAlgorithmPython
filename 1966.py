a73b683eb1463159baf9f959c0bb48677337fd6c
roxnook233 <87252115+roxnook233@users.noreply.github.com>
2024-08-24 00:40:13 +0900

    1966.py

--- 1966.py
+++ 1966.py
@@ -5,17 +5,20 @@ c = int(input())
 for i in range(c):
     N, M = map(int, input().split())
     prilis = list(map(int, input().split()))
+    mPos = M
     res = 0
-
     while True: 
         if max(prilis) == prilis[0]:
             prilis.pop(0)
             res += 1
-            if M == 0: break
+            if mPos == 0: break
+            mPos -= 1
         else:
             prilis.append(prilis[0])
-            M = len(prilis) - 1
             prilis.pop(0)
-        
+            mPos -= 1
+            if mPos < 0: mPos = len(prilis) -1
+# 1234 4123 123 321
+# 2 1 0 3 2 1 0
         
     print(res)
