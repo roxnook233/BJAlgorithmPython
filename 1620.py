74f4280a85df4ef729e7a96fedfd292c3e9554e1
roxnook233 <87252115+roxnook233@users.noreply.github.com>
2024-08-27 23:36:20 +0900

    Update 1620.py

--- 1620.py
+++ 1620.py
@@ -14,4 +14,24 @@ for i in range(M):
         print(lis[int(inp) - 1])
     else:
         print(dic[inp])
-        
+
+
+'''
+# 메모리 절약
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().strip().split())
+pokemon_dict = {}
+
+# 포켓몬 이름과 번호를 딕셔너리에 저장
+for i in range(1, N + 1):
+    pokemon_name = input().strip()
+    pokemon_dict[pokemon_name] = i
+    pokemon_dict[str(i)] = pokemon_name  # 번호를 문자열로 저장
+
+# 쿼리 처리
+for _ in range(M):
+    inp = input().strip()
+    print(pokemon_dict[inp])
+'''
