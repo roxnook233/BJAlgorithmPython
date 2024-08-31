610f2861cb9100ae48ce46a56f7d816b5de286a0
roxnook233 <87252115+roxnook233@users.noreply.github.com>
2024-08-31 20:13:03 +0900

    1463.py

---
+++ 1463.py
@@ -0,0 +1,19 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+# BJ 1463
+
+N = int(input())
+
+dp = [0, 0, 1, 1]
+i = 4
+while len(dp) <= N:
+    chklis =[]
+    if i % 3 == 0: 
+        chklis.append(dp[i // 3] + 1)
+    elif i % 2 == 0:
+        chklis.append(dp[i // 2] + 1)
+    chklis.append(dp[i-1] + 1)
+
+    dp.append(min(chklis))
+    i += 1
+print(dp[N])
